383fe142ebc6c982ae1b41f27cf6831d0183ec4e
Rosmari Nikova <rosmari.nikova@yahoo.com>
2019-06-26 20:06:35 +0300

    Add JediGalaxy problem

--- WorkingWithAbstraction/P03_JediGalaxy/Program.cs
+++ WorkingWithAbstraction/P03_JediGalaxy/Program.cs
@@ -1,65 +1,98 @@
-﻿using System;
-using System.Linq;
-
-namespace P03_JediGalaxy
+﻿namespace P03_JediGalaxy
 {
-    class Program
+    using System;
+    using System.Linq;
+
+    public class Program
     {
-        static void Main()
+        public static void Main()
         {
-            int[] dimestions = Console.ReadLine().Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
-            int x = dimestions[0];
-            int y = dimestions[1];
+            int[] dimestions = Console.ReadLine()
+                .Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
 
-            int[,] matrix = new int[x, y];
+            int row = dimestions[0];
+            int col = dimestions[1];
 
-            int value = 0;
-            for (int i = 0; i < x; i++)
-            {
-                for (int j = 0; j < y; j++)
-                {
-                    matrix[i, j] = value++;
-                }
-            }
+            int[,] matrix = new int[row, col];
+
+            FillTheMatrix(matrix, row, col);
 
             string command = Console.ReadLine();
-            long sum = 0;
+            long collectedStarsValue = 0;
+
             while (command != "Let the Force be with you")
             {
-                int[] ivoS = command.Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
-                int[] evil = Console.ReadLine().Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
-                int xE = evil[0];
-                int yE = evil[1];
+                int[] ivoCoordinates = command
+                    .Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
 
-                while (xE >= 0 && yE >= 0)
-                {
-                    if (xE >= 0 && xE < matrix.GetLength(0) && yE >= 0 && yE < matrix.GetLength(1))
-                    {
-                        matrix[xE, yE] = 0;
-                    }
-                    xE--;
-                    yE--;
-                }
+                int[] evilCoordinates = Console.ReadLine()
+                    .Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
 
-                int xI = ivoS[0];
-                int yI = ivoS[1];
+                MoveThroughTheStarsEvil(evilCoordinates, matrix);
 
-                while (xI >= 0 && yI < matrix.GetLength(1))
-                {
-                    if (xI >= 0 && xI < matrix.GetLength(0) && yI >= 0 && yI < matrix.GetLength(1))
-                    {
-                        sum += matrix[xI, yI];
-                    }
-
-                    yI++;
-                    xI--;
-                }
+                collectedStarsValue = MoveThroughTheStarsIvo(ivoCoordinates, matrix, collectedStarsValue);                              
 
                 command = Console.ReadLine();
             }
 
-            Console.WriteLine(sum);
+            Console.WriteLine(collectedStarsValue);
 
+        }
+
+        private static long MoveThroughTheStarsIvo(int[] ivoCoordinates, int[,] matrix, long collectedStarsValue)
+        {
+            int ivoRow = ivoCoordinates[0];
+            int ivoCol = ivoCoordinates[1];
+
+            while (ivoRow >= 0 && ivoCol < matrix.GetLength(1))
+            {
+                if (ivoRow < matrix.GetLength(0) && ivoCol >= 0)
+                {
+                    collectedStarsValue += matrix[ivoRow, ivoCol];
+                }
+
+                ivoCol++;
+                ivoRow--;
+            }
+
+            return collectedStarsValue;
+        }
+
+        private static int[,] MoveThroughTheStarsEvil(int[] evilCoordinates, int[,] matrix)
+        {
+            int evilRow = evilCoordinates[0];
+            int evilsCol = evilCoordinates[1];
+
+            while (evilRow >= 0 && evilsCol >= 0)
+            {
+                if (evilRow < matrix.GetLength(0) && evilsCol < matrix.GetLength(1))
+                {
+                    matrix[evilRow, evilsCol] = 0;
+                }
+                evilRow--;
+                evilsCol--;
+            }
+
+            return matrix;
+        }
+
+        private static void FillTheMatrix(int[,] matrix, int row, int col)
+        {
+            int curentNumber = 0;
+
+            for (int rows = 0; rows < row; rows++)
+            {
+                for (int cols = 0; cols < col; cols++)
+                {
+                    matrix[rows, cols] = curentNumber++;
+                }
+            }
         }
     }
 }
