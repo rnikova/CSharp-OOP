57254199cacab2af5d27aa88f323f0d240dfb00f
Rosmari Nikova <rosmari.nikova@yahoo.com>
2019-07-06 14:33:20 +0300

    Added ClassBoxData problem

---
+++ Encapsulation/ClassBoxData/Box.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassBoxData
+{
+    public class Box
+    {
+        private double length;
+        private double width;
+        private double height;
+
+        public Box(double length, double width, double height)
+        {
+            this.Length = length;
+            this.Width = width;
+            this.Height = height;
+        }
+
+        public double Length
+        {
+            get => this.length;
+            private set
+            {
+                ValidateParameters(value, "Length");
+
+                this.length = value;
+            }
+        }
+
+        public double Width
+        {
+            get => this.width;
+            private set
+            {
+                ValidateParameters(value, "Width");
+
+                this.width = value;
+            }
+        }
+
+        public double Height
+        {
+            get => this.height;
+            private set
+            {
+                ValidateParameters(value, "Height");
+
+                this.height = value;
+            }
+        }
+
+        private void ValidateParameters(double parameter, string parameterName)
+        {
+            if (parameter <= 0)
+            {
+                throw new Exception($"{parameterName} cannot be zero or negative.");
+            }
+        }
+
+        private double SurfaceArea()
+        {
+            double surfaceArea = 
+                2 * (this.Length * this.Width) 
+                + LateralSurfaceArea();
+
+            return surfaceArea;
+        }
+
+        private double LateralSurfaceArea()
+        {
+            double lateralSurfaceArea = 
+                2 * (this.Length * this.Height)
+                + 2 * (this.Width * this.Height);
+
+            return lateralSurfaceArea;
+        }
+
+        private double Volume()
+        {
+            double volume = this.Length * this.Width * this.Height;
+
+            return volume;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder result = new StringBuilder();
+
+            result.AppendLine($"Surface Area - {SurfaceArea():F2}");
+            result.AppendLine($"Lateral Surface Area - {LateralSurfaceArea():f2}");
+            result.AppendLine($"Volume - {Volume():f2}");
+
+            return result.ToString().TrimEnd();
+        }
+    }
+}
---
+++ Encapsulation/ClassBoxData/StartUp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ClassBoxData
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                double length = double.Parse(Console.ReadLine());
+                double width = double.Parse(Console.ReadLine());
+                double height = double.Parse(Console.ReadLine());
+
+                Box box = new Box(length, width, height);
+
+                Console.WriteLine(box);
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
